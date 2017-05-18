b14557f81102e1871f56bf90b99cc94dfaa05f4b
MasonHolland <masontholland@gmail.com>
2017-05-17 22:02:20 -0600

    passing iteration 3

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,25 @@
-require "pry"
 class ArtistsController < ApplicationController
-  
+
   def index
     @artists = Artist.all
   end
 
   def new
     @artist = Artist.new
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+
+    if @artist.update(artist_params)
+      redirect_to @artist
+    else
+      render :edit
+    end
   end
 
   def create
@@ -20,6 +33,12 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to artists_path
   end
 
   private
